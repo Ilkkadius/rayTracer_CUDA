56afdd4778a3e6e72f8221afc664700cb7fcb282
Ilkkadius <45123239+Ilkkadius@users.noreply.github.com>
2023-12-19 12:49:12 +0200

    Added light, camera and scenes

--- targetf.hpp
+++ targetf.hpp
@@ -9,8 +9,9 @@ public:
 
 Shape* shape;
 Vector3D color;
+float emissivity;
 
-    __device__ Target(Shape* shape_, const Vector3D& color_) : shape(shape_), color(color_) {}
+    __device__ Target(Shape* shape_, const Vector3D& color_, float emissivity_ = 0.0f) : shape(shape_), color(color_), emissivity(emissivity_) {}
 
     __device__ float collision(const Ray& ray) const {
         return shape->rayCollision(ray);
@@ -29,6 +30,14 @@ Vector3D color;
 
     __device__ void rotate(float angle, const Vector3D& axis, const Vector3D& axisPos) {
         shape->rotate(angle, axis, axisPos);
+    }
+
+    __device__ bool isRadiant() const {
+        return emissivity > 0.0f;
+    }
+
+    __device__ Vector3D emission() const {
+        return emissivity*color;
     }
 
 
