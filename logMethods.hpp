46bbd38c4570a7526d6b7a21a345b817887d8ff9
Ilkka <45123239+Ilkkadius@users.noreply.github.com>
2024-12-23 17:56:43 +0200

    Added methods for log keeping purposes

--- logMethods.hpp
+++ logMethods.hpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <sstream>
+#include <iomanip>
 
 __host__ std::string zero2front(int a)
 {
@@ -22,7 +23,7 @@ __host__ std::string getDate() {
     date >> mm >> dd >> yy;
 
     std::stringstream().swap(date);
-    date << dd << ". " << mm << ". " << yy << ": " << zero2front(h) << h << ":" << zero2front(m) << m << ":" << zero2front(s) << s << std::endl;
+    date << dd << ". " << mm << ". " << yy << ": " << zero2front(h) << h << ":" << zero2front(m) << m << ":" << zero2front(s) << s;
     //dd. <Month>. yyyy: hh:mm:ss
     return date.str();
 }
@@ -40,6 +41,23 @@ __host__ std::string getRawDate() {
     std::stringstream rawDate;
     rawDate << zero2front(dd) << dd << mm << yy - 2000 << "_" << zero2front(h) << h << zero2front(m) << m;
     return rawDate.str();
+}
+
+__host__ std::string getDuration(double duration, int precision = 2) {
+    std::stringstream ss;
+    int minutes = 0;
+    if(duration > 60) {
+        minutes = duration / 60;
+        ss << minutes << "min";
+    }
+    ss << std::fixed << std::setprecision(precision) << duration - 60.0 * minutes << "s";
+    return ss.str();
+}
+
+__host__ std::string getImageFilename(int width, int height, int samples, double duration) {
+    std::stringstream ss;
+    ss << width << "x" << height << "_" << samples << "samples_" << getDuration(duration);
+    return getRawDate() + "_GPU_" + ss.str() + "_figure.png";
 }
 
     
