014e8ff6635da12ac6a8982c530489759ebf6c9d
Ilkkadius <45123239+Ilkkadius@users.noreply.github.com>
2025-12-20 13:41:00 +0200

    Added first working CMakeLists

--- main.cu
+++ main.cu
@@ -58,7 +58,7 @@ int main(int argc, char *argv[]) {
     Camera cam;
 
     int width = 1920, height = 1080;
-    int depth = 4, samples = 1000;
+    int depth = 4, samples = 10;
     int tx = 8, ty = 8;
     bool backup = true;
     bool realTime = false;
