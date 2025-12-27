6d88e655d3f0f22c1bdf541662c591309e364469
Ilkkadius <45123239+Ilkkadius@users.noreply.github.com>
2025-12-27 13:51:13 +0200

    Added CSG reading from file

---
+++ src/io/csgRead.cu
@@ -0,0 +1,325 @@
+#include "csgRead.hpp"
+
+__host__ bool readVector3D(std::stringstream& ss, Vector3D& vec) { // TODO: Able to handle multiple lines
+    return (ss >> vec.x) && (ss >> vec.y) && ( ss >> vec.z);
+}
+
+__host__ bool csgRead::findNext(streamHolder sh, std::string& line, uint& linenum) {
+    if(sh.ss >> line) {
+        return true;
+    } else {
+        while(std::getline(sh.file, line)) {
+            linenum++; sh.ss.str(line); sh.ss.clear();
+            if(sh.ss >> line) return true;
+        }
+    }
+    return false;
+}
+
+// The following non-empty character must be '{'
+__host__ void csgRead::findStart(streamHolder sh, std::string& line, uint& linenum, affine& aff) {
+    findNext(sh, line, linenum); aux::uppercase(line);
+    if(line == "TRANSFORM") {
+        readTransformation(sh, line, linenum, aff);
+        findNext(sh, line, linenum);
+        if(line != "{") aux::error(sh.path, linenum, "CSG find start: Expected \"{\", found \"" + line + "\".");
+    } else if(line != "{") aux::error(sh.path, linenum, "CSG find start: Expected \"{\", found \"" + line + "\".");
+}
+
+// The following non-empty character must be '}'
+__host__ void csgRead::findEnd(streamHolder sh, std::string& line, uint& linenum) {
+    findNext(sh, line, linenum);
+    aux::uppercase(line);
+    if(line != "}") aux::error(sh.path, linenum, "CSG find end: Expected \"}\", found \"" + line + "\".");
+}
+
+__host__ void csgRead::skipTransformation(streamHolder sh, std::string& line, uint& linenum) {
+    findNext(sh, line, linenum); aux::uppercase(line);
+    if(line == "TRANSFORM") {
+        while(findNext(sh, line, linenum) && line != "}") {}
+        findNext(sh, line, linenum);
+        if(line != "{") aux::error(sh.path, linenum, "CSG skip transformation: Expected \"{\", found \"" + line + "\".");
+    } else if(line != "{") aux::error(sh.path, linenum, "CSG skip transformation: Expected \"{\", found \"" + line + "\".");
+}
+
+__host__ void csgRead::readTransformation(streamHolder sh, std::string& line, uint& linenum, affine& aff) {
+    findNext(sh, line, linenum);
+    if(line != "{") aux::error(sh.path, linenum, "CSG Transformation: Expected \"{\", found \"" + line + "\".");
+    bool ready = false;
+    while(findNext(sh, line, linenum)) {
+        aux::uppercase(line);
+        if(line == "MOVE") {
+            Vector3D b;
+            if(!readVector3D(sh.ss, b)) aux::error(sh.path, linenum, "CSG Transformation: Could not read translation vector.");
+            aff.b += b;
+        } else if(line == "ROTATE") {
+            Vector3D axis, pos; float angle;
+            if(!readVector3D(sh.ss, axis)) aux::error(sh.path, linenum, "CSG Transformation: Could not read axis.");
+            if(!(sh.ss >> angle)) aux::error(sh.path, linenum, "CSG Transformation: Could not read angle.");
+            if(!readVector3D(sh.ss, pos)) aux::error(sh.path, linenum, "CSG Transformation: Could not read axis position.");
+            Matrix rot = generateRotation(angle * M_PI/180.0f, unitVec(axis));
+            aff.A = rot * aff.A; aff.b = rot * aff.b;
+        } else if(line == "}") {
+            ready = true;
+            break;
+        }
+    }
+
+    if(!ready) aux::error(sh.path, linenum, "CSG Transformation: Definition does not end, forgotten \"}\"?");
+}
+
+__host__ bool csgRead::readTargets(const char* path, std::vector<targetData>& data, uint linenum) {
+    int counter = 1;
+    std::ifstream file(path);
+    std::string line; std::stringstream ss;
+    streamHolder sh{path, file, ss};
+    for(int i = 0; i < linenum; i++) std::getline(file,line);
+    bool ready = false;
+    while(std::getline(file, line)) {
+        linenum++;
+        ss.str(line); ss.clear();
+        if(ss >> line) {
+            aux::uppercase(line);
+            if(line == "SPHERE") {
+                Vector3D center, color;
+                float r, e;
+                skipTransformation(sh, line, linenum);
+                if(!readVector3D(ss, center)) aux::error(path, linenum, "Could not parse Sphere center.");
+                if(!(ss >> r)) aux::error(path, linenum, "Could not parse Sphere radius.");
+                if(!readVector3D(ss, color)) aux::error(path, linenum, "Could not parse Sphere color.");
+                if(!(ss >> e)) aux::error(path, linenum, "Could not parse Sphere emission.");
+                data.push_back({CSGTarget::SPHERE, center.x, center.y, center.z, r, color.x, color.y, color.z, e});
+                findEnd(sh, line, linenum);
+            } else if(line == "BOX") {
+                Vector3D color;
+                float a, b, c, e;
+                skipTransformation(sh, line, linenum);
+                if(!(ss >> a)) aux::error(path, linenum, "Could not parse Box x-width.");
+                if(!(ss >> b)) aux::error(path, linenum, "Could not parse Box y-width.");
+                if(!(ss >> c)) aux::error(path, linenum, "Could not parse Box z-width.");
+                if(!readVector3D(ss, color)) aux::error(path, linenum, "Could not parse Box color.");
+                if(!(ss >> e)) aux::error(path, linenum, "Could not parse Box emission.");
+                data.push_back({CSGTarget::BOX, a, b, c, color.x, color.y, color.z, e});
+                findEnd(sh, line, linenum);
+            } else if(line == "{") {
+                counter++;
+            } else if(line == "}") {
+                counter--;
+            }
+        }
+        while(ss >> line) {
+            if(line == "{") {
+                counter++;
+            } else if(line == "}") {
+                counter--;
+            }
+        }
+        if(counter <= 0) {
+            ready = true; break;
+        }
+    }
+    return ready;
+}
+
+__host__ void csgRead::processLeft(streamHolder sh, int ptr, std::string& line, std::vector<uint>& firstList, std::vector<CSG>& operList, std::vector<affine>& affines, int& nodeCounter, int& targetCounter, uint& linenum, affine aff) {
+    while(std::getline(sh.file, line)) {
+        linenum++; sh.ss.str(line); sh.ss.clear();
+        while(sh.ss >> line) {
+            aux::uppercase(line);
+            if(line[0] == '#') break;
+            else if(line == "UNION" || line == "INTERSECTION" || line == "DIFFERENCE") {
+
+                if(line == "UNION") operList[ptr] = CSG::UNION;
+                else if(line == "INTERSECTION") operList[ptr] = CSG::INTERSECTION;
+                else operList[ptr] = CSG::DIFFERENCE;
+
+                findStart(sh, line, linenum, aff);
+                
+                int first = nodeCounter; nodeCounter += 2;
+                firstList[ptr] = first;
+                csgRead::processLeft(sh, first, line, firstList, operList, affines, nodeCounter, targetCounter, linenum, aff);
+                csgRead::processRight(sh, first+1, line, firstList, operList, affines, nodeCounter, targetCounter, linenum, aff);
+
+                findEnd(sh, line, linenum);
+
+                return;
+
+            } else if(line == "SPHERE" || line == "BOX") {
+
+                findStart(sh, line, linenum, aff);
+
+                operList[ptr] = CSG::NONE;
+                firstList[ptr] = targetCounter;
+                affines[targetCounter++] = aff;
+
+                while(findNext(sh, line, linenum) && line != "}") {}
+
+                return;
+
+            }else {
+                aux::error(sh.path, linenum, "Could not parse \""+line+"\" as UNION/INTERSECTION/DIFFERENCE instance.");
+            }
+        }
+    }
+}
+
+__host__ void csgRead::processRight(streamHolder sh, int ptr, std::string& line, std::vector<uint>& firstList, std::vector<CSG>& operList, std::vector<affine>& affines, int& nodeCounter, int& targetCounter, uint& linenum, affine aff) {
+    while(std::getline(sh.file, line)) {
+        linenum++; sh.ss.str(line); sh.ss.clear();
+        while(sh.ss >> line) {
+            aux::uppercase(line);
+            if(line[0] == '#') break;
+            else if(line == "UNION" || line == "INTERSECTION" || line == "DIFFERENCE") {
+
+                if(line == "UNION") operList[ptr] = CSG::UNION;
+                else if(line == "INTERSECTION") operList[ptr] = CSG::INTERSECTION;
+                else operList[ptr] = CSG::DIFFERENCE;
+
+                findStart(sh, line, linenum, aff);
+                
+                int first = nodeCounter; nodeCounter += 2;
+                firstList[ptr] = first;
+                csgRead::processLeft(sh, first, line, firstList, operList, affines, nodeCounter, targetCounter, linenum, aff);
+                csgRead::processRight(sh, first + 1, line, firstList, operList, affines, nodeCounter, targetCounter, linenum, aff);
+
+                findEnd(sh, line, linenum);
+
+                return;
+
+            } else if(line == "SPHERE" || line == "BOX") {
+
+                findStart(sh, line, linenum, aff);
+
+                operList[ptr] = CSG::NONE;
+                firstList[ptr] = targetCounter;
+                affines[targetCounter++] = aff;
+
+                while(findNext(sh, line, linenum) && line != "}") {}
+
+                return;
+
+            }else {
+                aux::error(sh.path, linenum, "Could not parse \""+line+"\" as UNION/INTERSECTION/DIFFERENCE instance.");
+            }
+        }
+    }
+}
+
+__global__ void csgRead::generateCSG(TargetList** list, uint* firstList, CSG* operList, int nodeCount, targetData* data, affine* affines, int targetCount) {
+    if(threadIdx.x == 0 && blockIdx.x == 0) {
+
+        CSGNode* nodes = new CSGNode[nodeCount];
+        if(!nodes) printf("generateCSG: node allocation failed.\n");
+
+        TargetList* l = *list;
+
+        for(int i = 0; i < nodeCount; i++) {
+            nodes[i] = {firstList[i], 0, operList[i]};
+        }
+
+        Target** targets = new Target*[targetCount];
+        if(!targets) printf("generateCSG: target allocation failed.\n");
+        
+        for(int i = 0; i < targetCount; i++) {
+            CSGTarget type = data[i].type;
+            affine aff = affines[i];
+            float* p = data[i].params;
+            switch(type) {
+                case CSGTarget::SPHERE:
+                {
+                    Sphere* obj = new Sphere(Vector3D(p[0],p[1],p[2]),p[3],Vector3D(p[4],p[5],p[6]),p[7]);
+                    if(!obj) printf("generateCSG: Sphere allocation failed, i=%d.\n", i);
+                    obj->affine(aff.A, aff.b);
+                    targets[i] = obj;
+                    break;
+                }
+                case CSGTarget::BOX:
+                {
+                    Box* obj = new Box(p[0],p[1],p[2],Vector3D(p[3],p[4],p[5]),p[6]);
+                    if(!obj) printf("generateCSG: Box allocation failed, i=%d.\n", i);
+                    obj->affine(aff.A, aff.b);
+                    targets[i] = obj;
+                    break;
+                }
+            }
+        }
+        ConstructiveShape* obj = new ConstructiveShape(targets, targetCount, nodes, nodeCount);
+        if(!obj) printf("generateCSG: CSG shape allocation failed.\n");
+        l->targets[l->size++] = obj;
+    }
+}
+
+__host__ void csgRead::csgFromFile(const char* path, TargetList** list) {
+    std::ifstream file(path);
+    std::string line;
+    std::stringstream ss;
+    uint linenum = 0;
+
+    std::vector<uint> firstList;
+    std::vector<CSG> operList;
+    std::vector<targetData> targets;
+    std::vector<affine> affines;
+
+    streamHolder sh{path, file, ss};
+    affine aff{unitMatrix(), Vector3D(0.0f,0.0f,0.0f)};
+
+    while(std::getline(file, line)) {
+        linenum++;
+        ss.str(line); ss.clear(); // Removes previous error flags
+        while(ss >> line) {
+            aux::uppercase(line);
+            if(line[0] == '#') {
+                break;
+            } else if(line == "UNION" || line == "INTERSECTION" || line == "DIFFERENCE") {
+                if(!readTargets(path, targets, linenum)) aux::error(path, linenum, "Could not parse CSG object. Missing curly braces: \"{\", \"}\" ?");
+                if(targets.size() < 1) {
+                    file.close();
+                    return;
+                }
+                firstList.resize(2*targets.size()); operList.resize(2*targets.size());
+                affines.resize(targets.size());
+
+
+                firstList[0] = 1;
+                if(line == "UNION") operList[0] = CSG::UNION;
+                else if(line == "INTERSECTION") operList[0] = CSG::INTERSECTION;
+                else operList[0] = CSG::DIFFERENCE;
+
+                int nodeCounter = 3; int targetCounter = 0;
+
+                findStart(sh, line, linenum, aff);
+
+                processLeft(sh, 1, line, firstList, operList, affines, nodeCounter, targetCounter, linenum, aff);
+                processRight(sh, 2, line, firstList, operList, affines, nodeCounter, targetCounter, linenum, aff);
+
+                findEnd(sh, line, linenum);
+
+                file.close();
+
+                firstList.resize(nodeCounter); operList.resize(nodeCounter);
+
+                uint* firstList_d; CSG* operList_d; targetData* targets_d; affine* affines_d;
+                CHECK(cudaMalloc(&firstList_d, nodeCounter*sizeof(uint)));
+                CHECK(cudaMalloc(&operList_d, nodeCounter*sizeof(CSG)));
+                CHECK(cudaMalloc(&targets_d, targetCounter*sizeof(targetData)));
+                CHECK(cudaMalloc(&affines_d, targetCounter*sizeof(affine)));
+
+                CHECK(cudaMemcpy(firstList_d, firstList.data(), nodeCounter*sizeof(uint), cudaMemcpyHostToDevice));
+                CHECK(cudaMemcpy(operList_d, operList.data(), nodeCounter*sizeof(CSG), cudaMemcpyHostToDevice));
+                CHECK(cudaMemcpy(targets_d, targets.data(), targetCounter*sizeof(targetData), cudaMemcpyHostToDevice));
+                CHECK(cudaMemcpy(affines_d, affines.data(), targetCounter*sizeof(affine), cudaMemcpyHostToDevice));
+
+                generateCSG<<<1,1>>>(list, firstList_d, operList_d, nodeCounter, targets_d, affines_d, targetCounter);
+                CHECK(cudaDeviceSynchronize());
+
+                CHECK(cudaFree(firstList_d));
+                CHECK(cudaFree(operList_d));
+                CHECK(cudaFree(targets_d));
+                CHECK(cudaFree(affines_d));
+            }
+        }
+    }
+
+    
+
+}
---
+++ src/io/csgRead.hpp
@@ -0,0 +1,56 @@
+#ifndef CSG_READ_METHODS_CUDA_HPP
+#define CSG_READ_METHODS_CUDA_HPP
+
+#include <cuda_runtime.h>
+
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+#include "vector3D.hpp"
+#include "target.hpp"
+#include "kernelSet.hpp"
+
+namespace csgRead {
+
+    enum class CSGTarget {
+        SPHERE, // 8 floats
+        BOX // 7 floats
+    };
+
+    typedef struct {
+        CSGTarget type;
+        float params[8];
+    } targetData;
+
+    typedef struct {
+        const char* path;
+        std::ifstream& file;
+        std::stringstream& ss;
+    } streamHolder;
+
+    typedef struct {
+        Matrix A;
+        Vector3D b;
+    } affine;
+
+    __global__ void generateCSG(TargetList** list, uint* firstList, CSG* operList, int nodeCount, targetData* data, affine* affines, int targetCount);
+
+    __host__ bool findNext(streamHolder sh, std::string& line, uint& linenum);
+
+    __host__ void csgFromFile(const char* path, TargetList** list);
+
+    __host__ void processLeft(streamHolder sh, int ptr, std::string& line, std::vector<uint>& firstList, std::vector<CSG>& operList, std::vector<affine>& affines, int& nodeCounter, int& targetCounter, uint& linenum, affine aff);
+    __host__ void processRight(streamHolder sh, int ptr, std::string& line, std::vector<uint>& firstList, std::vector<CSG>& operList, std::vector<affine>& affines, int& nodeCounter, int& targetCounter, uint& linenum, affine aff);
+
+    __host__ bool readTargets(const char* path, std::vector<targetData>& data, uint linenum);
+
+    __host__ void readTransformation(streamHolder sh, std::string& line, uint& linenum, affine& aff);
+    __host__ void skipTransformation(streamHolder sh, std::string& line, uint& linenum);
+
+    __host__ void findStart(streamHolder sh, std::string& line, uint& linenum, affine& aff);
+    __host__ void findEnd(streamHolder sh, std::string& line, uint& linenum);
+
+};
+
+#endif
