49441f4a94ec9fb208f459e9a6e3bac73f2a4ed0
Ilkkadius <45123239+Ilkkadius@users.noreply.github.com>
2025-12-31 01:49:02 +0200

    Added operator<< for sceneType

--- src/scene/scenesf.hpp
+++ src/scene/scenesf.hpp
@@ -14,6 +14,30 @@ enum class sceneType {
     CSG2
 };
 
+__host__ inline std::ostream& operator<<(std::ostream& os, sceneType type) {
+    switch(type) {
+        case sceneType::EMPTY:
+            os << "EMPTY";
+            break;
+        case sceneType::PLATON:
+            os << "PLATON";
+            break;
+        case sceneType::LIGHT:
+            os << "LIGHT";
+            break;
+        case sceneType::CSG:
+            os << "CSG";
+            break;
+        case sceneType::CSG2:
+            os << "CSG2";
+            break;
+        case sceneType::TEST:
+            os << "TEST";
+            break;
+    }
+    return os;
+}
+
 namespace Scene{
 
     __host__ bool parseScene(std::string line, sceneType& type);
